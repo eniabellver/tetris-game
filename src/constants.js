176d413fde59b1448650f1407a1cb787151596c4
szabikr <szabikr.dev@gmail.com>
2021-12-29 18:56:07 +0000

    modularisation: build command with webpack

---
+++ src/constants.js
@@ -0,0 +1,9 @@
+/*--------------------
+GAME CONSTANTS & RULES
+--------------------*/
+
+export const BOARD_WIDTH = 10; // width in blocks
+export const BOARD_HEIGHT = 20; // height in blocks
+export const SCORE = 0;
+
+export const KEY = { UP: 38, DOWN: 40, LEFT: 37, RIGHT: 39, P: 80 }; // key event codes
