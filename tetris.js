be950e65d11f5fcb836e94ee05acf0ee293929b1
eniabellver <eugeniabellver@gmail.com>
2021-12-29 15:28:14 +0000

    created game board table

--- tetris.js
+++ tetris.js
@@ -14,43 +14,44 @@ const BOARD_WIDTH = 10; //width in blocks
 const BOARD_HEIGHT = 20; //height in blocks
 const SCORE = 0;
 
+//temporarily commented PIECE out until I need it
 //prettier-ignore
-const PIECE = [
-    // I SHAPE
-    [0,0,0,0,
-     1,1,1,1,
-     0,0,0,0,
-     0,0,0,0],
+// const PIECE = [
+//     // I SHAPE
+//     [0,0,0,0,
+//      1,1,1,1,
+//      0,0,0,0,
+//      0,0,0,0],
 
-    // L SHAPE
-    [1,0,0,
-     1,1,1,
-     0,0,0],
+//     // L SHAPE
+//     [1,0,0,
+//      1,1,1,
+//      0,0,0],
 
-    // J SHAPE
-    [0,0,1,
-     1,1,1,
-     0,0,0],
+//     // J SHAPE
+//     [0,0,1,
+//      1,1,1,
+//      0,0,0],
 
-    // T SHAPE
-    [0,1,0,
-     1,1,1,
-     0,0,0],
+//     // T SHAPE
+//     [0,1,0,
+//      1,1,1,
+//      0,0,0],
 
-    // O SHAPE
-    [1,1,
-     1,1],
+//     // O SHAPE
+//     [1,1,
+//      1,1],
 
-    // S SHAPE
-    [0,1,1,
-     1,1,0,
-     0,0,0],
+//     // S SHAPE
+//     [0,1,1,
+//      1,1,0,
+//      0,0,0],
 
-    // Z SHAPE
-    [1,1,0,
-     0,1,1,
-     0,0,0]
-];
+//     // Z SHAPE
+//     [1,1,0,
+//      0,1,1,
+//      0,0,0]
+// ];
 
 /*---------------------------------------
 CALCULATE SCALE AND FILL CANVAS RECTANGLE
@@ -61,10 +62,25 @@ ctx.scale(20, 20);
 ctx.fillStyle = '#000';
 ctx.fillRect(0, 0, canvas.width, canvas.height);
 
+/*--------
+GAME BOARD
+--------*/
+function createMatrix(width, height) {
+    let matrix = [];
+    while (height--) {
+        matrix.push(new Array(width).fill(0));
+    }
+    return matrix;
+}
+
+const BOARD = createMatrix(BOARD_WIDTH, BOARD_HEIGHT);
+console.log(BOARD);
+console.table(BOARD);
+
 /*---------------
 PLAY BUTTON EVENT
 ---------------*/
-const playButton = document.getElementById('play-btn');
+const playButton = document.getElementById('play');
 playButton.addEventListener('click', () => {
     //call the function that starts the game
 });
@@ -74,10 +90,6 @@ GAME FUNCTIONS
 ------------*/
 function start() {
     //function that resets/starts the board and hides play button
-}
-
-function reset() {
-    //clears the board, also is called on start()
 }
 
 function lose() {
